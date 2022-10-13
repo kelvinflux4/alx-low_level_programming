fc90c320f37a776604749dcc66c8fe0e277b48a2
kelvinflux4 <kflux@icloud.com>
2022-10-13 15:32:43 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 ( Success)
+ */
+int main(void)
+{
+	gets str = (Programming is like building a multilingual puzzle);
+
+	puts("%s\n", str);
+	return (0);
+}
