9bca5b31f0665b01b30073a3911fc993d984a87d
kelvinflux4 <kflux@icloud.com>
2022-10-14 15:52:28 -0700

    Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 48; num < 58; num++)
+	{
+		putchar(num);
+		if (num != 57)
+			continue;
+
+		putchar(' ');
+		putchar('$');
+	}
+	putchar('\n');
+	return (0);
+}
