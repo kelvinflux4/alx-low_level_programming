4f031299949ecc3733a5d4aa32683fe2d303018a
kelvinflux4 <kflux@icloud.com>
2022-10-18 01:41:35 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - check the code
+ *
+ * Return: Always o
+ */
+void print_alphabet(void)
+{
+	int i = a;
+
+	for (i = a; i <= z; i++)
+	{
+		putchar(i);
+		i++;
+	}
+}
