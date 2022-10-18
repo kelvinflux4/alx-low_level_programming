e0267e6d38859a356d19517bb846a779531416d7
kelvinflux4 <kflux@icloud.com>
2022-10-18 05:16:33 -0700

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _islower(int c) - checks the code
+ *
+ * Return: 0 success
+ */
+int _islower(int c)
+{
+	int num;
+
+	if (c >= 'a' && c <= 'z')
+	{
+		num 1;
+	}
+	else
+	{
+		num = 0;
+	}
+	return (num);
+}
