b98bc04004aac333b327f926cee1ceb6a07a095c
kelvinflux4 <kflux@icloud.com>
2022-10-18 02:22:38 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * _putchar - writes the character c to stdout
- * @c: parameter
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * main - entry point
+ * 
+ * Return: On success
+ * 
  */
-int _putchar(char c)
+int main(void)
 {
 	_putchar('_');
 	_putchar('p');
