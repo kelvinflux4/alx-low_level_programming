913ce50bcd20bb790f05f733e0762990f2787955
kelvinflux4 <kflux@icloud.com>
2022-10-23 20:55:18 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isupper - check the code
+ * @c: the variable to be checked
+ * Return: 1 if variable isuppercase 0 otherwise
+ */
+int _isupper(int c)
+{
+	if (isupper(c))
+{
+	return (1);
+}
+else
+{
+	return (0);
+}
+}
