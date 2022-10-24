4ddaeaf159b4216fc1486304bc52fad2fa51c6bd
kelvinflux4 <kflux@icloud.com>
2022-10-24 04:54:07 -0700

    Collaboration is multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+  * mul - multiplies two values
+  *
+  * @a: character value
+  * @b: character value
+  *
+  * Return: 0
+  */
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+	return (result);
+}
